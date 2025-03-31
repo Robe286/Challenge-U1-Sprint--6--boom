a5b18a850a493110b603bc5d5745adf2a9dbaaa5
Roberto Blasco~ <roberza2202@gmail.com>
2025-03-31 02:58:08 +0200

    primeros pasos

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Usuario escoge número
+// generar un número aleatorio del 1 al 3 --> revisar mathFloor y mathRandom
+// cuenta atrás de 5 a 0, getElementByID para imprimir la cuenta atras --> id="countdown"
+// No se sabrá que número es hasta que pasen 5 segundos --> usar `setTimeout()` para generar la asincronía de 5 segundos
+
+
+const userInput = document.getElementById('userInput');
+const inputValue = userInput.value;
+
+console.log(inputValue)
