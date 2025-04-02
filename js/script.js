6e6f99243ec347c05dff42c27e2defdf6f47e071
Roberto Blasco~ <roberza2202@gmail.com>
2025-04-02 11:37:15 +0200

    Terminado con comentarios

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@
 // id="result" para imprimir los resultados
 
 
-let counter = 6;
-let userNumber = 0
+let counter = 6; // Variable global creada para generar el contador regresivo
+let userNumber = 0 // Variable global creada para asignar el valor elegido por el user
 
 const userInput = document.getElementById('userInput');
 const countdown = document.getElementById('countdown');
@@ -21,30 +21,30 @@ const getRandomNum = (min, max) => Math.floor(Math.random() * max + min)
 
 // Inicio con click
 
-userInput.addEventListener('change', () => {
-    userNumber = userInput.value;
+userInput.addEventListener('change', () => { //Para poder obtener la elección del user escuchamos cualquier cambio en el input de entrada de valores
+    userNumber = userInput.value; // para poder usar ese valor lo vamos asignando en una variable global let userNumber = 0
 })
 
 //Función para comenzar el juego:
 
 function startGame () {
     count()
-    const randomNumPromise = new Promise((resolve) =>{
+    const randomNumPromise = new Promise((resolve) => {
         setTimeout(() =>{
-            resolve(getRandomNum(1, 3))
-        }, 6000)
+            resolve(getRandomNum(1, 3))  // devuelve el numero aleatorio
+        }, 6000) // la promesa respondera/se resolvera al pasar 6 segundos
     })
     return randomNumPromise // Retorna la promesa para despúes sacarla continuando con .then()
 }
 
-startGame().then(number => {
-    let mensaje = ''
+startGame().then(number => {  // sacamos la promesa y concatenamos las siguientes acciones con .then()
+    let mensaje = ''  // Para no hacer dos innerHTML creamos una variable donde se reasignará el mensaje que resulte de la comparación
     if(number == userNumber) {
         mensaje = `¡Has salvado el mundo!. El número seleccionado es ${userNumber} y el número aleatorio es ${number}`
     } else {
         mensaje = `¡Has destruido el mundo!. El número seleccionado es ${userNumber} y el número aleatorio es ${number}`
     }
-    result.innerHTML = `<p>${mensaje}</p>`
+    result.innerHTML = `<p>${mensaje}</p>` // pintamos en el DOM un parrafo con el mensaje ya reasignado.
 })
 
 // Función cuenta atras:
@@ -52,13 +52,13 @@ startGame().then(number => {
 function count () {
     let id = setInterval(() => { 
         if(counter == 0) {
-            clearInterval(id);
+            clearInterval(id); // detiene el intervalo. Si no se detiene la funcion setInterval sige infinitamente.
         }
          else {
-            counter = counter - 1; 
-            countdown.innerHTML = counter; 
+            counter = counter - 1; // con -1 generamos la cuenta hacia atrás
+            countdown.innerHTML = counter; // pintamos el contador en el DOM
         }
-    }, 1000);
+    }, 1000); // cada segundo pinta en el DOM el número: 5, 4, 3, 2, 1, 0
 }
 
 //Funcion reiniciar juego:
